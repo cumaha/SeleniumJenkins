0d29af787470aa4f693a859c1c5659675fe8fa69
cumaha <cumaha@gmail.com>
2018-02-25 17:32:38 -0500

    Update FirstTest.java

--- src/test/java/com/test/FirstTest.java
+++ src/test/java/com/test/FirstTest.java
@@ -1,7 +1,7 @@
 package com.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class FirstTest {
@@ -9,8 +9,8 @@ public class FirstTest {
 @Test
 public void tc_(){
 	
-	WebDriver driver = new FirefoxDriver();
-	System.setProperty("webdriver.Firefoxdriver","C:\\Softwares\\Selenium\\Firefox\\geckodriver.exe");
+	System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\Selenium\\Firefox\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
 	driver.get("http://www.google.com");
 	driver.findElement(By.name("q")).sendKeys("Selenium");
 	
